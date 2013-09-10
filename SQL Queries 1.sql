bfac9c9cd1e6202bdcfafd36790d25ed665ca17f
Aaron Kippins <Aaron.Kippins1@marist.edu>
2013-09-10 16:04:34 -0400

    Questions 1-10 completed and tested, Still testing for possible errors.

---
+++ SQL Queries 1.sql
@@ -0,0 +1,30 @@
+-- SQL Queries 1
+-- Aaron Kippins
+-- Question 1 
+SELECT cid, name, city, discount FROM customers
+-- Question 2
+SELECT name, city FROM agents
+WHERE name = 'Smith'
+-- Question 3
+SELECT pid, name, quantity FROM products
+WHERE priceusd > '1.25'
+-- Question 4 
+SELECT ordno, aid FROM orders
+-- Question 5
+SELECT name, city FROM customers 
+WHERE city <> 'Dallas'
+-- Question 6
+SELECT name FROM agents
+WHERE (city = 'New York') OR (city = 'Newark')
+-- Question 7
+SELECT pid, name, city, quantity, priceUSD FROM products
+WHERE (city <> 'New York') OR (city <> 'Newark')
+-- Question 8
+SELECT ordno, mon, cid, aid, pid, qty, dollars FROM orders
+WHERE (mon = 'jan') OR (mon = 'mar')
+-- Question 9 
+SELECT ordno, mon, cid, aid, pid, qty, dollars FROM orders
+WHERE (mon = 'feb') AND (dollars < '100')
+-- Question 10
+SELECT ordno FROM orders
+WHERE cid = 'c005'
