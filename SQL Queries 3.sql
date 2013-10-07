df11a92060f27166b5882e03074edfca31a19af4
Aaron Kippins <Aaron.Kippins1@marist.edu>
2013-10-07 11:49:33 -0400

    Questions 1-8 done, 12, 13, 15, and 16 done. Still working on 9-11, 14, and 17.

--- SQL Queries 3.sql
+++ SQL Queries 3.sql
@@ -23,7 +23,7 @@ select distinct c.name
 from customers c, orders o
 where c.cid not in (select o.cid from orders o)
 
---Question 6 **** Come Back To This ****
+--Question 6 
 select *
 from customers c 
 left join orders o on c.cid = o.cid
@@ -42,16 +42,17 @@ from customers c
 full outer join agents a on c.city = a.city
 where c.city = a.city
 
---Question 9 
+--Question 9 *******
 select p.name, p.city
 from customers c
 full outer join products p on c.city = p.city
-where p.quantity in (select min(quantity) from products p)
+where p.city in (select count(city) from products p)
 
 --Question 10 ******
-select *
+select p.name, p.city
 from customers c
 full outer join products p on c.city = p.city
+where p.quantity in (select max(quantity) from products p)
 
 --Question 11 *****
 select *
@@ -64,11 +65,28 @@ from products p
 where p.priceUSD > (select avg(priceUSD)
 		    from products p)
 
---Question 13 ********
+--Question 13 
 select c.name, o.pid, o.dollars
 from customers c, orders o
-where 
+where c.cid = o.cid
+order by o.dollars desc
 
---Question 14 
-select *
-from 
+--Question 14 *********
+select c.name, coalesce(sum(o.dollars), 0.00)
+from orders o
+left outer join customers c on c.cid = o.cid
+
+--Question 15 
+select c.name, p.name, a.name
+from agents a, customers c, orders o, products p
+where a.aid = o.aid and c.cid = o.cid and p.pid = o.pid and a.city = 'New York'
+
+--Question 16
+select o.pid, o.qty * p.priceusd, o.dollars
+from orders o, products p
+where o.pid = p.pid
+
+--Question 17 *********
+select
+from
+where
